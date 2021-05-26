0fe4e323a1bab7c1f3050a1cadda9fd47ec29106
StefanBokarev <i_s.d.bokarev@mpt.ru>
2021-05-26 10:31:58 +0300

    Added serialization

---
+++ Impl/Models/Person.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Impl.Models
+{
+    public class Person
+    {
+        public string Id { get; set; }
+    }
+}
---
+++ Logster/Serialization/Serializer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Json;
+using System.Text;
+
+namespace Logster.Serialization
+{
+    public static class Serializer
+    {
+        public static T Deserialize<T>(string body)
+        {
+            using (var stream = new MemoryStream())
+            using (var writer = new StreamWriter(stream))
+            {
+                writer.Write(body);
+                writer.Flush();
+                stream.Position = 0;
+                return (T) new DataContractJsonSerializer(typeof(T)).ReadObject(stream);
+            }
+        }
+        
+        public static string Serialize<T>(T item)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                Type t = typeof(T);
+                new DataContractJsonSerializer(typeof(T)).WriteObject(ms, item);
+                var result = Encoding.Default.GetString(ms.ToArray());
+                return string.IsNullOrEmpty(result) ? String.Empty : result;
+            }
+        }
+    }
+}
